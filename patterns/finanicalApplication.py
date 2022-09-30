e6e6e219ee0be5aaf1a483b4e5d35532821b7b88
AbhishekPassavula <passavulaabhishek@gmail.com>
2022-09-30 20:15:33 +0530

    spell correction

--- patterns/finanicalApplication.py
+++ patterns/finanicalApplication.py
@@ -1,15 +1,15 @@
-def calcualate_investment(deposit_cash,intrest,months):
+def calcualate_investment(deposit_cash,interest,months):
     amount=deposit_cash
     i=1
     while(i<=months):
-        amount=amount+(amount*(intrest/100)/12)
+        amount=amount+(amount*(interest/100)/12)
         i+=1
     
     return "{:.2f}".format(amount)
 
 if __name__=="__main__":
     deposit_cash=int(input("Enter CD's purchase value ($):"))
-    intrest = float(input("Enter CD's annual percentage yield (%):"))
+    interest = float(input("Enter CD's annual percentage yield (%):"))
     months  = int(input("Enter CD's investment term (months):"))
 
-    print(f"Your CD is worth ${calcualate_investment(deposit_cash,intrest,months)} at the end of the {months} months term")
+    print(f"Your CD is worth ${calcualate_investment(deposit_cash,interest,months)} at the end of the {months} months term")
